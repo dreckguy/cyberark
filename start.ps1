78ec4fe897b9c3e2056a8a7967bf4c622c8026c8
ophirus <dreckguy@gmail.com>
2021-01-10 09:24:28 +0200

    remove ps script since not working

--- start.ps1
+++
@@ -1,4 +0,0 @@
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
-terraform apply -auto-approve
-echo db_ip=$(terraform output db_ip) >> compose/.env
-ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$(terraform output web-app-1_ip) "docker-compose -f /home/ubuntu/docker-compose.yml pull"
