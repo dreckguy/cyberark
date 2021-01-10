70a91095436d2c4976ff5a4fa0ad06c5bf503463
ophirus <dreckguy@gmail.com>
2021-01-10 12:11:45 +0200

    fix linux script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 terraform apply -auto-approve
 terraform output | tr -d " "\" > compose/.env
-scp -i key.pem -o "StrictH ostKeyChecking no" compose/.env ubuntu@$(terraform output web-app-1_ip | tr -d " "\"):/home/ubuntu/
-ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$(terraform output web-app-1_ip |) "docker-compose -f /home/ubuntu/docker-compose.yml pull"
+export WEB_APP_1_IP=$(terraform output web-app-1_ip| tr -d " "\")
+scp -i key.pem -o "StrictH ostKeyChecking no" compose/.env ubuntu@$WEB_APP_1_IP:/home/ubuntu/
+ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml up"
