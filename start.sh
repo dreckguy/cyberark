24a1d8b0e72635370623b4b34a77a9762b01392b
ophirus <dreckguy@gmail.com>
2021-01-10 09:20:58 +0200

    store output in compose env file format

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 terraform apply -auto-approve
-echo db_ip=$(terraform output db_ip) >> compose/.env
+terraform output | tr -d " "\" > compose/.env
 scp -i key.pem -o "StrictHostKeyChecking no" compose/.env ubuntu@$(terraform output web-app-1_ip):/home/ubuntu/
 ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$(terraform output web-app-1_ip) "docker-compose -f /home/ubuntu/docker-compose.yml pull"
