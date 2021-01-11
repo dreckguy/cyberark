f8032ce80169853a72b796b8ab9acb9eb2cc5610
ophirus <dreckguy@gmail.com>
2021-01-11 14:06:06 +0200

    split execs

--- start.sh
+++ start.sh
@@ -7,7 +7,8 @@ export DB_IP=$(terraform output db_ip| tr -d " "\")
 
 ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$DB_IP "docker run -p 8529:8529 -e ARANGO_NO_AUTH=1 arangodb"
 scp -i key.pem -o "StrictH ostKeyChecking no" compose/.env ubuntu@$WEB_APP_1_IP:/home/ubuntu/
-ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml pull; \
-docker-compose -f /home/ubuntu/docker-compose.yml run app init.js; docker-compose up"
+ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml pull"
+ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml run app init.js"
+ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml docker-compose up"
 
 
