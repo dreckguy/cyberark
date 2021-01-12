3ccd6df5c0e7ff48eb0390fcb5cc2bd60181f38c
ophirus <dreckguy@gmail.com>
2021-01-12 12:21:14 +0200

    fix newline

--- start.sh
+++ start.sh
@@ -13,5 +13,5 @@ ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compos
 
 sleep 2m
 echo "reading directors:"
-curl "$LB_DNS/directors"
+curl "$LB_DNS/directors\n"
 echo "Ready to show famous directors and their films on:\n$LB_DNS/directors"
