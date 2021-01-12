68d5ec3e089c7262bcd5261dfd3c908e91727057
ophirus <dreckguy@gmail.com>
2021-01-12 22:26:29 +0200

    secure http connection from outside the subnet

--- network.tf
+++ network.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "default" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.1.0/24"]
   }
   # HTTP Arangodb access from the VPC
 
@@ -72,15 +72,7 @@ resource "aws_security_group" "default" {
     from_port   = 8529
     to_port     = 8529
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.1.0/24"]
   }
 }
 
--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,16 @@ output "web-app-1_ip" {
   value = aws_instance.web-app-1.public_ip
 }
 
-output "db_ip" {
+output "db_pubic_ip" {
   description = "The public ip for ssh access"
   value = aws_instance.db.public_ip
 }
+
+output "db_private_ip" {
+  description = "The public ip for ssh access"
+  value = aws_instance.db.private_ip
+}
+
 
 output "lb_dns" {
   description = "dns of the elastic load balancer"
