9920ff3a144a4703e1154fde0ed919766d363582
ophirus <dreckguy@gmail.com>
2021-01-12 10:52:38 +0200

    remove explicit pull

--- start.sh
+++ start.sh
@@ -8,8 +8,9 @@ export LB_DNS=$(terraform output lb_dns| tr -d " "\")
 echo "starting db"
 ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$DB_IP "docker run -p 8529:8529 -e ARANGO_NO_AUTH=1 -d arangodb"
 scp -i key.pem -o "StrictHostKeyChecking no" compose/.env ubuntu@$WEB_APP_1_IP:/home/ubuntu/
-ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml pull"
 ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml run app init.js"
 ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@$WEB_APP_1_IP "docker-compose -f /home/ubuntu/docker-compose.yml up -d"
+
+sleep 2s
 echo "reading directors:"
 curl "$LB_DNS/directors"
